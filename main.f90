646a372d72588fe7541c5e5911457449083d0b4c
ttpekkan <timo_pekkanen@hotmail.com>
2012-10-23 18:11:01 +0300

    neljäs

--- main.f90
+++ main.f90
@@ -1,82 +1,49 @@
-program t3
+
+program t4
     implicit none
-    character(len = 512) :: inputWord
-    character(len = :), allocatable :: word, temporaryWord
-    character(len = 50), dimension(:), allocatable :: palindromeArray
-    integer :: wordLength, palindromeFunction, palindromeCounter, point, i, j, k
+    integer, dimension(:,:), allocatable :: numberArray
+    integer :: squareSize, counter, i, j
 
-    write(*,'(a)', advance = 'no') 'Anna Merkkijono: '
-    read(*,'(a)') inputWord
-    wordLength = len_trim(inputWord)
-    word = inputWord(1:wordLength)
+    write(*,'(a)', advance = 'no') 'Anna koko: '
+    read(*,*) squareSize
 
-    palindromeCounter = 0
-    temporaryWord = ''
+    allocate(numberArray(squareSize, squareSize))
 
-    do i = 1, wordLength  !Tämä vasta laskee palindromien määrän, jotta voidaan tehdä oikean kokoinen array.
-        point = i
-        do j = i, wordLength
-            temporaryWord = temporaryWord // word(point:point)
-            point = point + 1
-            if (palindromeFunction(temporaryWord, len(temporaryWord)) == 1) then
-                palindromeCounter = palindromeCounter + 1
-            end if
-        end do
-        temporaryWord = ''
+    do i = 1, squareSize
+        if (mod(i,2) == 0) then
+            counter = i**2
+            do j = 1, i
+                numberArray(i,j) = counter
+                counter = counter - 1
+            end do
+            counter = (i - 1)**2 + 1
+            do j = 1, i - 1
+                numberArray(j,i) = counter
+                counter = counter + 1
+            end do
+        else
+            counter = (i - 1)**2 + 1
+            do j = 1, i
+                numberArray(i,j) = counter
+                counter = counter + 1
+            end do
+            counter = i**2
+            do j = 1, i - 1
+                numberArray(j,i) = counter
+                counter = counter - 1
+            end do
+        end if
+
+
     end do
 
-    allocate(palindromeArray(palindromeCounter))
-    palindromeCounter = 1
-    temporaryWord = ''
-
-    do i = 1, wordLength  !Tämä samannäköinen looppi sitten lisää sanat listaan. Tarkistaa myös mahdolliset toistot.
-        point = i
-        do j = i, wordLength
-            temporaryWord = temporaryWord // word(point:point)
-            point = point + 1
-            if (palindromeFunction(temporaryWord, len(temporaryWord)) == 1) then
-                do k = 1, palindromeCounter
-                    if (palindromeArray(k) == temporaryWord) then
-                        goto 45
-                    end if
-                end do
-                palindromeArray(palindromeCounter) = temporaryWord
-                palindromeCounter = palindromeCounter + 1
-45         end if
+    do i = 1, squareSize
+        do j = 1, squareSize
+            write(*,'(i5)', advance = 'no') numberArray(i,j) !Isoilla luvuilla printtiä pitää vähän säätää.
         end do
-        temporaryWord = ''
-    end do
-
-    write(*,'(i8)',advance = 'no') (palindromeCounter - 1) !Numero ei tulostu, jos palindromeja on yli 8-numeroinen luku
-    write(*,'(a)') ' palindromia'
-    do i = 1, wordLength                !Kaksi looppia tarpeellista vain, jos haluaa listata sanat kokojärjestyksessä.
-        do j = 1, palindromeCounter - 1
-            if (len_trim(palindromeArray(j)) == i) then
-                write(*,*) palindromeArray(j)
-            end if
-        end do
+         write(*,*)
+         write(*,*)
     end do
 
 end program
-
-function palindromeFunction(givenWord, givenLength) result(isPalindrome)
-    implicit none
-    integer :: givenLength, isPalindrome, startPoint
-    character(len = givenLength) :: givenWord, testWord
-
-    testWord = ''
-    startPoint = givenLength
-    do while (startPoint > 0)
-        testWord = trim(testWord) // givenWord(startPoint:startPoint)
-        startPoint = startPoint - 1
-    end do
-    if (testWord == givenWord) then
-        isPalindrome = 1
-    else
-        isPalindrome = -1
-    end if
-end function palindromeFunction
-
-
-
 
