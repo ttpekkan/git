194b32f15fb58d230f52028569b34377386ff32c
ttpekkan <timo_pekkanen@hotmail.com>
2012-10-28 21:38:32 +0200

    seitsemäs

--- main.f90
+++ main.f90
@@ -1,65 +1,52 @@
+
 program hello
     implicit none
-    character(len = 512) :: initialChain
-    integer :: chainSize, point, i, counter
+    integer :: givenNumber, i, point, counter
+    integer, dimension(:), allocatable :: array
 
-    write(*, '(a)', advance = 'no') 'Anna pituus: '
-    read(*,*) chainSize
-    point = chainSize
-    initialChain = ''
-    counter = 1
+    write(*,'(a)', advance = 'no') 'Anna luku: '
+    read(*,*) givenNumber
+    allocate(array(givenNumber))
+    do i = 1, givenNumber
+        array(i) = 0
+    end do
+    point = 1
+    counter = 0
 
-    do i = 1, chainSize
-        initialChain = trim(initialChain) // 'A'
+    do
+        counter = 0                     !Piti tehdä taas paloissa rekursio, ettei tule segmentation faulttia.
+        call modifyArray                !Ei kyllä mitään hajua mikä sen aiheuttaa.
     end do
 
-    write(*,*) trim(initialChain)
-    do
-        call changeLetter(initialChain) !Ohjelma suljetaan muualta. Rekursio pitää suorittaa pienissä osissa
-    end do                              !että ei tule segmentation faulttia.
+contains
 
-contains !Containissa olevat subroutiinit päävevät käsiksi ja voivat muuttaa alussa annettuja muuttujia.
-
-    recursive subroutine changeLetter(chain)
-        character(len = 512) :: chain
-
-        if(counter > 20) then           !Rekursio tehdään 20-sanan pätkissä, yllä mainitusta syystä.
-            counter = 0                 !Nyt voidaan laskea mikä tahansa pituus, tosin jos pituus yli 10,
-            initialChain = chain        !homma melko hidasta. Ketjun pituus tosin voi olla max 512, mutta
-            return                      !tätä voi tietenkin helposti muuttaa.
-        end if
-        if(chain(point:point) == 'A') then
-            chain(point:point) = 'C'
-            call recurse(chain)
-        else if(chain(point:point) == 'C') then
-            chain(point:point) = 'G'
-            call recurse(chain)
-        else if(chain(point:point) == 'G') then
-            chain(point:point) = 'T'
-            call recurse(chain)
-        else
-            chain(point:point) = 'A'
-            point = point - 1
-            if (point == 0) then
-                stop                    !Lopettaa ohjelman.
-            end if
-            call changeLetter(chain)
-        end if
-
-    end subroutine changeLetter
-
-    subroutine recurse(chain)
-        character(len = 512) :: chain
-
-        write(*,*) trim(chain)
+    recursive subroutine modifyArray
         counter = counter + 1
-        if (point < chainSize) then
-            point = chainSize
-            call changeLetter(chain)
-        else
-            call changeLetter(chain)
+        if(counter > 20) then
+            return
         end if
-
-    end subroutine recurse
+        if(array(point) == 0) then
+            array(point) = 1
+            do i = 1, givenNumber
+                if(array(i) == 1) then
+                    write(*,'(i8)', advance = 'no') i
+                end if
+            end do
+            write(*,*)
+            if(point > 1) then
+                point = 1
+                call modifyArray
+            end if
+            call modifyArray
+        else
+            array(point) = 0
+            point = point + 1
+            if (point > givenNumber) then
+                stop                !Lopettaa ohjelman.
+            end if
+            call modifyArray
+        end if
+    end subroutine modifyArray
 
 end program
+
