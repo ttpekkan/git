6724ae0dcd3c8032db47a5a42d1e51d807720a85
ttpekkan <timo_pekkanen@hotmail.com>
2012-10-29 07:42:05 +0200

    kahdeksas

--- main.f90
+++ main.f90
@@ -1,52 +1,31 @@
-
-program hello
+program t8
     implicit none
-    integer :: givenNumber, i, point, counter
-    integer, dimension(:), allocatable :: array
+    character(len = 30) :: numberChain
+    integer :: givenNumber, i
 
-    write(*,'(a)', advance = 'no') 'Anna luku: '
+    write(*, '(a)', advance = 'no') 'Anna pituus: '
     read(*,*) givenNumber
-    allocate(array(givenNumber))
-    do i = 1, givenNumber
-        array(i) = 0
-    end do
-    point = 1
-    counter = 0
-
-    do
-        counter = 0                     !Piti tehdä taas paloissa rekursio, ettei tule segmentation faulttia.
-        call modifyArray                !Ei kyllä mitään hajua mikä sen aiheuttaa.
-    end do
+    numberChain = ''
+    call buildChain(givenNumber, givenNumber)
 
 contains
 
-    recursive subroutine modifyArray
-        counter = counter + 1
-        if(counter > 20) then
-            return
-        end if
-        if(array(point) == 0) then
-            array(point) = 1
-            do i = 1, givenNumber
-                if(array(i) == 1) then
-                    write(*,'(i8)', advance = 'no') i
+    recursive subroutine buildChain(point, length)
+        integer :: point, length
+        if (point < 1) then
+            do i = 1, length
+                if(numberChain(i:i) == '1') then
+                    write(*,'(i3)', advance = 'no') i
                 end if
             end do
             write(*,*)
-            if(point > 1) then
-                point = 1
-                call modifyArray
-            end if
-            call modifyArray
-        else
-            array(point) = 0
-            point = point + 1
-            if (point > givenNumber) then
-                stop                !Lopettaa ohjelman.
-            end if
-            call modifyArray
+            return
         end if
-    end subroutine modifyArray
+        numberChain(point:point) = '0'
+        call buildChain(point-1, length)
+        numberChain(point:point) = '1'
+        call buildChain(point-1, length)
+    end subroutine buildChain
 
 end program
 
