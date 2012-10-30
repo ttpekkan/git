7e31017cb3cab12732440314bc9e8c4b04d9a304
ttpekkan <timo_pekkanen@hotmail.com>
2012-10-30 22:33:05 +0200

    !

--- main.f90
+++ main.f90
@@ -1,7 +1,7 @@
 
 program hello
     implicit none
-integer, dimension(5,5) :: board
+    integer, dimension(5,5) :: board
     integer :: i, j, total
     common /a/ total
 
@@ -9,8 +9,8 @@ integer, dimension(5,5) :: board
         do j = 1, 5
             board(i,j) = 0
         end do
-end do
-total = 0
+    end do
+    total = 0
 
     call moveKnight(1, 1, 1, board, 0)
     write(*,*) total
@@ -20,65 +20,65 @@ end program
 
 recursive subroutine moveKnight(i, j, counter, board)
     implicit none
-integer :: i, j, counter, checkIfOutside, total
+    integer :: i, j, counter, checkIfOutside, total
     integer, dimension(5,5) :: board
     common /a/ total
 
     board(i,j) = counter
     if(counter == 25) then
-total = total + 1
+        total = total + 1
         call printBoard(board)
     end if
 
-if(checkIfOutside(i+2, j+1) == -1) then
-if(board(i+2, j+1) == 0) then
-call moveKnight(i+2, j+1, counter+1, board)
+    if(checkIfOutside(i+2, j+1) == -1) then
+        if(board(i+2, j+1) == 0) then
+            call moveKnight(i+2, j+1, counter+1, board)
         end if
-end if
+    end if
 
-if(checkIfOutside(i+1, j+2) == -1) then
-if(board(i+1, j+2) == 0) then
-call moveKnight(i+1, j+2, counter+1, board)
+    if(checkIfOutside(i+1, j+2) == -1) then
+        if(board(i+1, j+2) == 0) then
+            call moveKnight(i+1, j+2, counter+1, board)
         end if
-end if
+    end if
 
-if(checkIfOutside(i-1, j+2) == -1) then
-if(board(i-1, j+2) == 0) then
-call moveKnight(i-1, j+2, counter+1, board)
+    if(checkIfOutside(i-1, j+2) == -1) then
+        if(board(i-1, j+2) == 0) then
+            call moveKnight(i-1, j+2, counter+1, board)
         end if
-end if
+    end if
 
-if(checkIfOutside(i-2, j+1) == -1) then
-if(board(i-2, j+1) == 0) then
-call moveKnight(i-2, j+1, counter+1, board)
+    if(checkIfOutside(i-2, j+1) == -1) then
+        if(board(i-2, j+1) == 0) then
+            call moveKnight(i-2, j+1, counter+1, board)
         end if
-end if
+    end if
 
-if(checkIfOutside(i-1, j-2) == -1) then
-if(board(i-1, j-2) == 0) then
-call moveKnight(i-1, j-2, counter+1, board)
+    if(checkIfOutside(i-1, j-2) == -1) then
+        if(board(i-1, j-2) == 0) then
+            call moveKnight(i-1, j-2, counter+1, board)
         end if
-end if
+    end if
 
-if(checkIfOutside(i+2, j-1) == -1) then
-if(board(i+2, j-1) == 0) then
-call moveKnight(i+2, j-1, counter+1, board)
+    if(checkIfOutside(i+2, j-1) == -1) then
+        if(board(i+2, j-1) == 0) then
+            call moveKnight(i+2, j-1, counter+1, board)
         end if
-end if
+    end if
 
-if(checkIfOutside(i+1, j-2) == -1) then
-if(board(i+1, j-2) == 0) then
-call moveKnight(i+1, j-2, counter+1, board)
+    if(checkIfOutside(i+1, j-2) == -1) then
+        if(board(i+1, j-2) == 0) then
+            call moveKnight(i+1, j-2, counter+1, board)
         end if
-end if
+    end if
 
-if(checkIfOutside(i-2, j-1) == -1) then
-if(board(i-2, j-1) == 0) then
-call moveKnight(i-2, j-1, counter+1, board)
+    if(checkIfOutside(i-2, j-1) == -1) then
+        if(board(i-2, j-1) == 0) then
+            call moveKnight(i-2, j-1, counter+1, board)
         end if
-end if
+    end if
 
-board(i,j) = 0
+    board(i,j) = 0
 end subroutine moveKnight
 
 subroutine printBoard(board) !Kaikin puolin tyylikäs ratkaisu tulostukseen.
@@ -88,61 +88,61 @@ subroutine printBoard(board) !Kaikin puolin tyylikäs ratkaisu tulostukseen.
     do i = 1, 5
         do j = 1, 5
             if(board(i,j) == 1) then
-write(*,'(a)', advance = 'no') 'A'
+                write(*,'(a)', advance = 'no') 'A'
             else if(board(i,j) == 2) then
-write(*,'(a)', advance = 'no') 'B'
+                write(*,'(a)', advance = 'no') 'B'
             else if(board(i,j) == 3) then
-write(*,'(a)', advance = 'no') 'C'
+                write(*,'(a)', advance = 'no') 'C'
             else if(board(i,j) == 4) then
-write(*,'(a)', advance = 'no') 'D'
+                write(*,'(a)', advance = 'no') 'D'
             else if(board(i,j) == 5) then
-write(*,'(a)', advance = 'no') 'E'
+                write(*,'(a)', advance = 'no') 'E'
             else if(board(i,j) == 6) then
-write(*,'(a)', advance = 'no') 'F'
+                write(*,'(a)', advance = 'no') 'F'
             else if(board(i,j) == 7) then
-write(*,'(a)', advance = 'no') 'G'
+                write(*,'(a)', advance = 'no') 'G'
             else if(board(i,j) == 8) then
-write(*,'(a)', advance = 'no') 'H'
+                write(*,'(a)', advance = 'no') 'H'
             else if(board(i,j) == 9) then
-write(*,'(a)', advance = 'no') 'I'
+                write(*,'(a)', advance = 'no') 'I'
             else if(board(i,j) == 10) then
-write(*,'(a)', advance = 'no') 'J'
+                write(*,'(a)', advance = 'no') 'J'
             else if(board(i,j) == 11) then
-write(*,'(a)', advance = 'no') 'K'
+                write(*,'(a)', advance = 'no') 'K'
             else if(board(i,j) == 12) then
-write(*,'(a)', advance = 'no') 'L'
+                write(*,'(a)', advance = 'no') 'L'
             else if(board(i,j) == 13) then
-write(*,'(a)', advance = 'no') 'M'
+                write(*,'(a)', advance = 'no') 'M'
             else if(board(i,j) == 14) then
-write(*,'(a)', advance = 'no') 'N'
+                write(*,'(a)', advance = 'no') 'N'
             else if(board(i,j) == 15) then
-write(*,'(a)', advance = 'no') 'O'
+                write(*,'(a)', advance = 'no') 'O'
             else if(board(i,j) == 16) then
-write(*,'(a)', advance = 'no') 'P'
+                write(*,'(a)', advance = 'no') 'P'
             else if(board(i,j) == 17) then
-write(*,'(a)', advance = 'no') 'Q'
+                write(*,'(a)', advance = 'no') 'Q'
             else if(board(i,j) == 18) then
-write(*,'(a)', advance = 'no') 'R'
+                write(*,'(a)', advance = 'no') 'R'
             else if(board(i,j) == 19) then
-write(*,'(a)', advance = 'no') 'S'
+                write(*,'(a)', advance = 'no') 'S'
             else if(board(i,j) == 20) then
-write(*,'(a)', advance = 'no') 'T'
+                write(*,'(a)', advance = 'no') 'T'
             else if(board(i,j) == 21) then
-write(*,'(a)', advance = 'no') 'U'
+                write(*,'(a)', advance = 'no') 'U'
             else if(board(i,j) == 22) then
-write(*,'(a)', advance = 'no') 'V'
+                write(*,'(a)', advance = 'no') 'V'
             else if(board(i,j) == 23) then
-write(*,'(a)', advance = 'no') 'W'
+                write(*,'(a)', advance = 'no') 'W'
             else if(board(i,j) == 24) then
-write(*,'(a)', advance = 'no') 'X'
+                write(*,'(a)', advance = 'no') 'X'
             else
-write(*,'(a)', advance = 'no') 'Y'
+                write(*,'(a)', advance = 'no') 'Y'
             end if
-          ! write(*,'(i3)', advance = 'no') board(i,j)
+            ! write(*,'(i3)', advance = 'no') board(i,j)
         end do
-write(*,*)
+        write(*,*)
     end do
-write(*,*)
+    write(*,*)
     write(*,*)
 end subroutine printBoard
 
@@ -150,9 +150,9 @@ function checkIfOutside(i, j) result(outside)
     integer :: i, j, outside
 
     if (i > 5 .or. i < 1 .or. j > 5 .or. j < 1) then
-outside = 1
+        outside = 1
     else
-outside = -1
+        outside = -1
     end if
 end function checkIfOutside
 
