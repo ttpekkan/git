7a095cda2e6bdf845114395e41bc8158bca25b73
ttpekkan <timo_pekkanen@hotmail.com>
2012-11-13 05:36:05 +0200

    kokeilu

--- main.f90
+++ main.f90
@@ -1,49 +1,74 @@
 
-program t13
+program hello
     implicit none
-    integer, dimension(25) :: primes
-    integer*16, dimension(100) :: array
-    integer :: inputNumber, i
-    integer*16 :: total, sumOfPrimeSums
-    common/a/ array, primes
+    character(len = 100) :: inputWord, startChar
+    character(len = 1), dimension(26) :: chars
+    integer, dimension(26) :: location
+    integer :: i, counter
+    common/a/ inputWord, counter
 
-    primes = (/2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97/)
-    write(*,'(a)', advance = 'no') 'Anna luku: '
-    read(*,*) inputNumber
-    total = 0
+    write(*,'(a)', advance = 'no') 'Anna merkkijono: '
+    read(*,*) inputWord
 
-    do i = 1, 25
-        if(primes(i) == inputNumber) then
-            cycle
+    call uniqueChars(inputWord, chars, location)
+    do i = 1, 26
+        if(chars(i) == '') then
+            exit
         end if
-        total = total + sumOfPrimeSums((inputNumber-primes(i)))
+        startChar = inputWord(i:i)
+        call getSubstrings(startChar, location(i))
     end do
-    write(*,*) 'Tulos: ', total
+    write(*,*) counter
 
 end program
 
-recursive function sumOfPrimeSums(someNumber) result(total)
-    integer, dimension(25) :: primes
-    integer*16, dimension(100) :: array
-    integer :: someNumber, i
-    integer*16 :: total
-    common/a/ array, primes
+recursive subroutine getSubstrings(substring, point)
+    implicit none
+    character(len = 100) :: substring, inputWord, nextWord
+    character(len = 1), dimension(26) :: chars
+    integer, dimension(26) :: location
+    integer :: point, i, counter
+    common/a/ inputWord, counter
 
-    total = 0
-    if(someNumber < 0) then
+    if(substring == inputWord) then
         return
     end if
-    if(someNumber == 0) then
-        total = 1
+    counter = counter + 1
+    write(*,*) substring
+    if(point == len_trim(inputWord)) then
         return
     end if
-    if(array(someNumber) /= 0) then
-        total = array(someNumber)
-        return
-    end if
-    do i = 1, 25
-        array(someNumber) = array(someNumber) + sumOfPrimeSums((someNumber-primes(i)))
+    call uniqueChars(inputWord(point+1:len_trim(inputWord)), chars, location)
+    do i = 1, 26
+        if(chars(i) == '') then
+            exit
+        end if
+        nextWord = trim(substring) // chars(i)
+        call getSubstrings(nextWord, location(i)+point)
     end do
-    total = array(someNumber)
 
-end function sumOfPrimeSums
+end subroutine getSubstrings
+
+subroutine uniqueChars(someString, chars, location)
+    character(len = 100), intent(in) :: someString
+    character(len = 1), dimension(26), intent(out) :: chars
+    integer, dimension(26), intent(out) :: location
+    integer :: i, j, counter
+
+    counter = 0
+    chars = ''
+    location = 0
+    do i = 1, len_trim(someString)
+        do j = 1, counter
+            if(someString(i:i) == chars(j)) then
+                goto 999
+            end if
+        end do
+        counter = counter + 1
+        chars(counter) = someString(i:i)
+        location(counter) = i
+        999 continue
+    end do
+end subroutine uniqueChars
+
+
