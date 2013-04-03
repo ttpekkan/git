d9e3691cdd51495b116c06187d4c497d6835e514
ttpekkan <timo_pekkanen@hotmail.com>
2013-04-04 01:09:08 +0300

    f4

--- main.f90
+++ main.f90
@@ -1,8 +1,16 @@
+!Kuumatka-ohjelma
+
+!Periaatteesahan koodi on niin selvää, ettei sitä tarvitsisi kommentoida, mutta jos nyt tämän kerran.
+
+!Ohjelma on melko hidas (noin 0.5 h), koska käytetyt aika-askeleet ja nopeusaskeleet ovat melko pieniä ja sisäkkäisiä
+!silmukoita on muutama. Aika-askelia ja nopeusaskelia kasvattamalla ohjelma tietenkin nopeutuu huomattavasti.
 
 program f4
     implicit none
+    real*8 :: earthMass, moonMass, gravitationalConstant, endPoint, earthMoonDistance, moonRadius, earthRadius, KarmanLine
+    real*8 :: timeInterval, minVelocity
 
-    real*8 :: interval, earthMass, moonMass, gravitationalConstant, endPoint, earthMoonDistance, moonRadius, earthRadius, KarmanLine
+    !Ohjelmassa käytetyt vakiot.
 
     gravitationalConstant = 6.67259e-11
     moonMass = 7.348e22
@@ -12,38 +20,77 @@ program f4
     KarmanLine = 100*1000
     earthMoonDistance = 384400*1000
 
-    endPoint = earthMoonDistance - moonRadius
+    endPoint = earthMoonDistance - moonRadius   !Kuun pinta.
+    minVelocity = 12200                         !Ensimmäinen arvaus minimilähtönopeudelle on maan pakonopeus + 1000 m/s.
+    timeInterval = 120                          !Aika-askel, jota myöhemmin piennenetään.
 
-    interval = 0.1          !Aika-askel
+    !Silmukka laskee minimilähtönopeuden tietyllä aika-askeleella ja kirjoittaa tiedostoon aika-askeleen ja
+    !sitä vastaavaan lähtönopeuden arvon. Tiedoston pohjalta voidaan myöhemmin muodostaa kuvaaja. Koska
+    !aika-askeleen pienentäminen aina pienentää lähtönopeutta, niin ensimmäinen arvaus lähtönopeudelle on
+    !aina edellinen tulos lähtönopeudelle. Tämä lyhentää laskuaikaa huomattavasti. Ensimmäinen aika-askel on
+    !120 sekuntia. Silmukka lopettaa laskemisen, kun aika-askel on ~0.01 s. Lopuksi vielä laskee
+    !lähtönopeuden, kun aika-askel on tarkalleen 0.01 s.
 
-    call calculateMinSpeed(interval)
+    open(unit = 1, file = 'alkunopeusAika-askeleenFunktiona')
+    do while(timeInterval > 0.01)
+        minVelocity = calculateMinVelocity(timeInterval, minVelocity)
+        write(1,*) timeInterval, minVelocity
+        timeInterval = timeInterval - 0.01
+    end do
+    close(1)
+    timeInterval = 0.01
+    minVelocity = calculateMinVelocity(timeInterval, minVelocity)
+
+    !Seuraava kohta laskee ja tulostaa matka-ajan ja lähtönopeuden, kun lähtönopeus on minimilähtönopeus ja
+    !kun lähtönopeus on 1.1-kertaa minimilähtönopeus.
+
+    write(*,*) 'Aika-askel = 0.01 s'
+    write(*,*) 'Lähtönopeus:', minVelocity, 'Matka-aika:', calculateTime(timeInterval, minVelocity)/(60*60)
+    write(*,*) 'Lähtönopeus:', 1.1*minVelocity, 'Matka-aika:',calculateTime(timeInterval, 1.1*minVelocity)/(60*60)
+
+    !Silmukka kirjoittaa tiedostoon lähtönopeuden ja sitä vastaavan matka-ajan. Ensimmäinen lähtönopeus on
+    !minimilähtönopeus ja viimeinen lähtönopeus on ~20000 m/s. Lähtönopeutta kasvatetaan aina 1 m/s.
+
+    open(unit = 1, file = 'matka-AikaAlkunopeudenFunktiona')
+    do while (minVelocity < 20000)
+        write(1,*) minVelocity, calculateTime(timeInterval, minVelocity)/(60*60)
+        minVelocity = minVelocity + 1.0
+    end do
+    close(1)
+
 
 contains
 
-    subroutine calculateMinSpeed(interval)
-        real*8 :: interval, initialVelocity
+    !Funktiolle annetaan aika-askel ja arvaus minimilähtönopeudelle. Arvaus on aina enemmän kun varsinainen
+    !minimilähtönopeus. Funktiossa oleva silmukka pienentää lähtönopeutta niin kauan, kunnes ei enää päästä
+    !kuuhun. Sitten funktio palauttaa viimeisen lähtönopeuden, jolla päästiin kuuhun. Lähtönopeutta
+    !pienennetään aina 0.01 m/s.
 
-        open(unit = 1, file = 'nopeudetJaAjat')
-        write(1,*) 'Alkunopeus', '               ',  'Matka-aika (tunteina)'
+    function calculateMinVelocity(timeInterval, velocityGuess) result(minVelocity)
+        real*8 :: timeInterval, velocityGuess, minVelocity, previousVelocity
 
-        initialVelocity = 10950                            !Lähtee kokeilemaan alkunopeuksia tästä arvosta.
+        minVelocity = velocityGuess
+
         do
-            if(calculateTime(initialVelocity, interval) > 0) then   !Ottaa ylös tuloksen, jos päästään kuuhun.
-                write(1,*) initialVelocity, calculateTime(initialVelocity, interval)/(60*60)
+            if(calculateTime(timeInterval, minVelocity) < 0) then   !Jos aika < 0, tarkoittaa, ettei päästä kuuhun.
+                minVelocity = previousVelocity
+                return
             end if
-            if(initialVelocity >= 12200) then                !Lopettaa nopeuksien laskemisen.
-                exit
-            end if
-            initialVelocity = initialVelocity + 0.1          !Nopeus-askel
+            previousVelocity  = minVelocity
+            minVelocity = minVelocity - 0.01
         end do
-        close(1)
 
-    end subroutine calculateMinSpeed
+    end function calculateMinVelocity
 
+    !Funktio laskee, päästäänkö annetulla lähtönopeudella ja aika-askeleella kuuhun ja jos päästään,
+    !niin funktio palauttaa matka-ajan. Jos ei päästä, funktio palauttaa negatiivisen arvon matka-
+    !ajalle. Funktiossa oleva silmukka päivittää raketin paikkaa, nopeutta ja kiihtyvyyttä jokaisen
+    !aika-askeleen jälkeen. Silmukka tarkastaa myös, ollaanko perillä ja mennäänkö joka askeleella
+    !eteenpäin. Jos ei mennä, silmukka ymmärtää lopettaa laskemisen.
 
-    function calculateTime(initialVelocity, interval) result(time)
-        real*8 :: time, initialVelocity, interval, velocity
-        real*8 :: lastDistance, distanceFromEarth, distanceFromMoon, acceleration
+    function calculateTime(timeInterval, initialVelocity) result(time)
+        real*8 :: timeInterval, initialVelocity
+        real*8 :: acceleration, velocity, time, distanceFromEarth, distanceFromMoon, lastDistance
 
         time = 0.0
         distanceFromEarth = earthRadius + KarmanLine
@@ -52,23 +99,21 @@ contains
         lastDistance = distanceFromEarth
 
         do
-            acceleration = gravitationalConstant * (moonMass/distanceFromMoon**2 - earthMass/distanceFromEarth**2)
-            distanceFromEarth = distanceFromEarth + velocity*interval + 0.5*acceleration*interval**2
+            acceleration = gravitationalConstant * (moonMass/distanceFromMoon**2 - earthMass/distanceFromEarth**2)  !yhtälö 4
+            distanceFromEarth = distanceFromEarth + velocity*timeInterval + 0.5*acceleration*timeInterval**2        !yhtälö 5
             distanceFromMoon = earthMoonDistance - distanceFromEarth
-            time = time + interval
-            if(distanceFromEarth >= endPoint) then
+            time = time + timeInterval
+            if(distanceFromEarth >= endPoint) then          !Perillä, palauttaa matka-ajan.
                 return
             end if
-            if(distanceFromEarth <= lastDistance) then  !Ei pääse perille, lopettaa laskemisen.
+            if(distanceFromEarth <= lastDistance) then      !Ei pääse perille, lopettaa laskemisen.
                 time = -1.0
                 return
             end if
-            velocity = velocity + acceleration*interval
+            velocity = velocity + acceleration*timeInterval !yhtälö 6
             lastDistance = distanceFromEarth
         end do
 
     end function calculateTime
-
 end program
-
 
