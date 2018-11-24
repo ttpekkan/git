8c0fc7f3fe3308ebc7f85f047ec17b4feda64639
ttpekkan <timo.t.pekkanen@helsinki.fi>
2018-11-24 15:35:19 +0200

    heatEquation

--- heatEquation.f90
+++ heatEquation.f90
@@ -5,7 +5,7 @@ program heatEquation
 
     integer(kind = iprec) :: i, j, meshSize, tLim
     real(kind = rprec) :: a, radius, timeLimit, dr, dt, r, t
-    real(kind = rprec), dimension(:), allocatable :: M      !temperature matrix (time, radial coordinate, temperature)
+    real(kind = rprec), dimension(:), allocatable :: M, Mprev      !temperature matrix (time, radial coordinate, temperature)
 
     a = 18.5d-6                          !alpha [m**2 s**-1]
     radius = 4.0d-3                      !m
@@ -17,10 +17,11 @@ program heatEquation
     tLim = IDNINT(timeLimit/dt)          !number of time steps.
 
     allocate(M(meshSize+1))              !initialise matrix
+    allocate(Mprev(meshSize+1))
     M = 650.0d0                          !set initial gas temperature
     M(meshSize+1) = 550.0d0              !set initial surface temperature
 
-    open(unit = 11, file = 'output2D.dat', action = 'write')
+    open(unit = 11, file = 'output3D.dat', action = 'write')
     open(unit = 12, file = 'outputAxial.dat', action = 'write')
 
     t = 0.0d0
@@ -31,21 +32,22 @@ program heatEquation
 
     do j = 1, tLim                                      !solve dT/dt = alpha*[d**2 U/dr**2 + (1/r)dU/dr] (angular term is zero)
         t = t + dt
+        Mprev = M
         do i = 1, meshSize+1
             r = (i-1)*radius/meshSize
             if(i == 1) then                                     !dT/dt = alpha*[d**2 U/dr**2 + (1/r)dU/dr] at r=0
-                M(1) = M(1) + 4*a*dt*(M(2) - M(1))/(dr**2)
+                M(1) = Mprev(1) + 4*a*dt*(Mprev(2) - Mprev(1))/(dr**2)
             else if (i == meshSize+1) then                      !do nothing if at boundary (surface)
                 continue
             else                                                !dT/dt = alpha*[d**2 U/dr**2 + (1/r)dU/dr] at r /= 0
-                M(i) = M(i) &
-                        + (a*dt)*(M(i+1) - 2*M(i) + M(i-1))/(dr**2) &
-                        + (a*dt/r)*(M(i+1) - M(i-1))/(2*dr)
+                M(i) = Mprev(i) &
+                        + (a*dt)*(Mprev(i+1) - 2*Mprev(i) + Mprev(i-1))/(dr**2) &
+                        + (a*dt/r)*(Mprev(i+1) - Mprev(i-1))/(2*dr)
             end if
             if(mod(j,tLim/1000) == 0) then                      !don't save every point.
                 write(11,*) t, (i-1)*radius/meshSize, M(i)
                 if(i==1) then
-                    write(12,*) t, M(i)
+                    write(12,*) t, M(1)
                 end if
             end if
         end do
